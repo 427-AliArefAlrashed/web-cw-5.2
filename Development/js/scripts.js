720b84a0f49a0509020e610c9803fc16f2feab25
Alia2003a <45044538+Alia2003a@users.noreply.github.com>
2020-06-29 22:17:21 +0300

    done

---
+++ Development/js/scripts.js
@@ -0,0 +1,72 @@
+
+
+let food = prompt("ما تحب ان تطلب ؟");
+
+    switch(food) {
+        case "1" :
+        console.log("pizza");
+            break;
+        case "2" :
+            console.log("burger");
+            break;
+        case "3":
+            console.log("pasta");
+            break;    
+        default:
+            console.log("none");
+            break;
+
+    };
+
+let number1 =prompt("ادخل رقم")
+
+ switch(number1){
+     case "4" :
+         break;
+     default :
+     alert(error);
+     
+ }
+let number2 =prompt("ادخل رقم")
+
+ switch(number2){
+     case "6" :
+         break;
+     default :
+     alert(error);
+ }
+let cal =prompt("اختر (+ , / , * , -)")
+
+
+ switch(cal){
+     case "+" :
+         console.log( 4 + 6 )
+         break;
+     case "-" :
+         console.log(4-6)
+         break;     
+     case "*" :
+         console.log(4*6)  
+     case "/" :
+         console.log(4/6)
+ }
+
+ let age=prompt( "اكتب عمرك")
+
+ if(age<= 5 ){
+     console.log("الروضة")
+ }
+ if(age <=10 ){
+    console.log("ابتدائي")
+}
+if(age <=14 ){
+    console.log("متوسط")
+}
+if(age <=18 ){
+    console.log("ثانوي")
+}
+if(age >18 ){
+    console.log("جامعة")
+}
+
+
